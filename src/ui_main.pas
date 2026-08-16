4226a2634a5dbf22292346e4f6e73b66ea271477
PhoenixCampbell <phjcampbell18@gmail.com>
2026-08-15 21:27:54 -0500

    add styling to center text on menu

--- src/ui_main.pas
+++ src/ui_main.pas
@@ -1,8 +1,7 @@
 unit ui_main;
 
 interface
-uses shogi_game, ai_opponent, crt;
-
+uses shogi_game, ai_opponent, crt; // Add 'crt' to use functions like GetMaxX and GetMaxY
 procedure MainMenu();
 function GetUserInput: string;
 procedure SinglePlayerGame();
@@ -11,13 +10,45 @@ procedure DisplayRules();
 
 implementation
 
-procedure MainMenu();
+// Function to center a line of text horizontally
+procedure CenterText(const Text: string);
+var
+  ScreenWidth, ScreenHeight: integer;
 begin
-  WriteLn('Shogi Game - Main Menu');
-  WriteLn('1. Single Player vs AI');
-  WriteLn('2. Player vs Player');
-  WriteLn('3. Display Rules');
-  WriteLn('4. Exit');
+  ScreenWidth := GetMaxX;
+  ScreenHeight := GetMaxY;
+  ClrScr;
+
+  // Calculate horizontal and vertical center positions
+  GotoXY((ScreenWidth div 2) - (Length(Text) div 2), (ScreenHeight div 2));
+  WriteLn(Text);
+end;
+
+// Function to write a line of text at the specified vertical offset from the center
+procedure WriteLine(const Text: string; VerticalOffset: integer);
+var
+  ScreenWidth, ScreenHeight: integer;
+begin
+  ScreenWidth := GetMaxX;
+  ScreenHeight := GetMaxY;
+
+  // Calculate horizontal and vertical positions
+  GotoXY((ScreenWidth div 2) - (Length(Text) div 2), (ScreenHeight div 2) + VerticalOffset);
+  WriteLn(Text);
+end;
+
+procedure MainMenu();
+var
+  LineOffsets: array[1..4] of integer = (-3, 0, 3, 6); // Adjust line offsets as needed
+
+begin
+  CenterText('Shogi Game - Main Menu');
+
+  WriteLine('1. Single Player vs AI', LineOffsets[1]);
+  WriteLine('2. Player vs Player', LineOffsets[2]);
+  WriteLine('3. Display Rules', LineOffsets[3]);
+  WriteLine('4. Exit', LineOffsets[4]);
+
   case GetUserInput() of
     '1': SinglePlayerGame();
     '2': PlayerVsPlayer();
@@ -54,14 +85,23 @@ begin
 end;
 
 procedure DisplayRules();
+const
+  PieceNames: array[TPiece] of string = ('None', 'Pawn', 'Lance', 'Knight', 'SilverGeneral', 'GoldGeneral',
+                                         'PromotedSilverGeneral', 'PromotedKnight', 'PromotedLance',
+                                         'PromotedPawn', 'Bishop', 'Rook', 'DragonHorse', 'DragonKing');
+
 var
   i: byte;
 begin
   ClrScr;
-  WriteLn('Shogi Game Rules:');
-  WriteLn('------------------');
+
+  CenterText('Shogi Game Rules:');
+
   for i := Low(TPiece) to High(TPiece) do
-    WriteLn(PieceValue[i], ' - ', TPiece(i));
+  begin
+    WriteLine(PieceNames[i] + ': ' + IntToStr(PieceValue[i]), (i - Low(TPiece)) * 2);
+  end;
+
   ReadKey;
 end;
 
